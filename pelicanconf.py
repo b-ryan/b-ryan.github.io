4d67feb2e3710b8d121e4790e9cde780e29abf59
Buck Ryan <buck.ryan@gmail.com>
2013-11-20 00:55:13 -0500

    blah

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,3 +30,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
