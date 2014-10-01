c3a63e9bcc819dd152e6bb0f3e1329fcdfa4a4fe
Buck Ryan <fryan@rjmetrics.com>
2014-10-01 18:15:17 -0400

    change develop.sh to assume virtualenvwrapper

--- develop.sh
+++ develop.sh
@@ -1,6 +1,6 @@
 (
   mkdir output
   cd output
-  ../.venv/bin/python -m pelican.server 8080
+  python -m pelican.server 8080
 ) &
-.venv/bin/pelican --debug --autoreload -t theme/ -o output/ -s pelicanconf.py content/
+pelican --debug --autoreload -t theme/ -o output/ -s pelicanconf.py content/
