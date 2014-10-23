53599b0401569bca610006c26aa276174d1ccab2
Buck Ryan <fryan@rjmetrics.com>
2014-10-22 23:09:04 -0400

    update root

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://buckryan.com'
+SITEURL = 'http://b-ryan.github.io/buckryan.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
