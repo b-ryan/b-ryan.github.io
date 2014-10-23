4fb665f8466fb07595516c5dd6b0a0d56647f758
Buck Ryan <fryan@rjmetrics.com>
2014-10-22 23:00:46 -0400

    start deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-pelican -t theme/ -o output/ -s publishconf.py content/
-aws --region us-west-2 s3 sync output s3://buckryan.com/
+if [[ $(git status --short | egrep -v '^\?\?' | wc -l) -gt 0 ]]; then
+    echo "Git status is dirty. You must have a clean git to deploy." >&2
+    exit 1
+fi
+
+commit="$(git rev-parse HEAD)"
+echo "deploying $commit"
+
+output_dir=/tmp/output
+
+echo "Compiling to $output_dir"
+pelican -t theme -o $output_dir -s publishconf.py content
+
+echo "Committing to gh-pages branch"
+git checkout gh-pages
+git rm -r .
+rsync -r $output_dir/ .
+
+rm -r $output
