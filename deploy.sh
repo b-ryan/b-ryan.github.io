663dbdd042688643919a3013a57681bdcb05fe09
Buck Ryan <fryan@rjmetrics.com>
2014-10-22 21:36:13 -0400

    remove everything but index.html

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pelican -t theme/ -o output/ -s publishconf.py content/
-aws --region us-west-2 s3 sync output s3://buckryan.com/
--- develop.sh
+++
@@ -1,6 +0,0 @@
-(
-  mkdir output
-  cd output
-  python -m pelican.server 8080
-) &
-pelican --debug --autoreload -t theme/ -o output/ -s pelicanconf.py content/
