3de20d1bf644320851e4d4b07e6747c212e63c28
Buck Ryan <fryan@rjmetrics.com>
2014-10-22 23:02:42 -0400

    take 1

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ pelican -t theme -o $output_dir -s publishconf.py content
 echo "Committing to gh-pages branch"
 git checkout gh-pages
 git rm -r .
+git reset --
 rsync -r $output_dir/ .
 
-rm -r $output
+rm -r $output_dir
